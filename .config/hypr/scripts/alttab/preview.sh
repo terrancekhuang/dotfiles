faf45725f76badf082d4d99fc468b61c2ec5eaae
Terrance Huang <terrancehuang@proton.me>
2025-10-20 18:26:16 -0400

    just formatting

--- .config/hypr/scripts/alttab/preview.sh
+++ .config/hypr/scripts/alttab/preview.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 line="$1"
 
-IFS=$'\t' read -r addr _ <<< "$line"
+IFS=$'\t' read -r addr _ <<<"$line"
 dim=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}
 
 grim -t png -l 0 -w "$addr" ~/.config/hypr/scripts/alttab/preview.png
