ed6a9eb186e9fbb8bfd7e2f60ead9dda0fd77195
Terrance Huang <terrancehuang@proton.me>
2025-10-24 17:14:08 -0400

    nvim autocomplete and snippets

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,12 +1,17 @@
 return {
   {
     "mason-org/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
     end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "ts_ls" },
@@ -15,11 +20,20 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.ts_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
