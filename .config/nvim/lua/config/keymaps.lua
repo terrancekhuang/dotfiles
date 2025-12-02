e82e81de4aa95945163d6b741db143776f3e3c9c
Terrance Huang <terrancehuang@proton.me>
2025-12-01 19:42:32 -0500

    insert mode - shift tab for unindent

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,5 @@ vim.keymap.set("v", "J", "mzJ`z", opts)
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', opts)
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', opts)
 vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', opts)
+
+vim.keymap.set("i", "<S-Tab>", "<C-d>", opts)
