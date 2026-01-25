7ae13f12b0dc2ee3d3c89e130510cddc249928c0
Terrance Huang <terrancehuang@proton.me>
2026-01-25 12:03:42 -0500

    nvim - added centered half page scrolling keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,8 @@ local opts = {
 }
 
 vim.keymap.set("n", "J", "mzJ`z", opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 vim.keymap.set("n", "n", "nzzzv", opts)
 vim.keymap.set("n", "N", "Nzzzv", opts)
 
