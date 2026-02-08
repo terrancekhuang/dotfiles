13dcd35dca067ffc48872e171e3f52179905d299
Terrance Huang <terrancehuang@proton.me>
2026-02-08 01:07:05 -0500

    added go to references keybind

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -37,6 +37,7 @@ return {
 
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
     end,
   },
 }
