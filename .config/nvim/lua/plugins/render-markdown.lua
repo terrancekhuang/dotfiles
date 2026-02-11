344b798c4fef1f27a00b7d791af7f49a72203287
Terrance Huang <terrancehuang@proton.me>
2026-02-11 14:08:52 -0500

    render latex nvim

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -4,5 +4,14 @@ return {
     checkbox = {
       enabled = true,
     },
+    latex = {
+      enabled = true,
+      render_modes = false,
+      converter = { "utftex", "latex2text" },
+      highlight = "RenderMarkdownMath",
+      position = "center",
+      top_pad = 0,
+      bottom_pad = 0,
+    },
   },
 }
