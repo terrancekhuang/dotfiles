81577902748f67ffc2f50b81554d21731df54d25
Terrance Huang <terrancehuang@proton.me>
2026-08-23 18:58:02 -0400

    markdown preview config fix hopefully

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,8 +1,13 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    config = function()
-      vim.g.mkdp_auto_close = 0
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_auto_close = 0 -- don't close preview when switching buffers
+      vim.g.mkdp_combine_preview = 0 -- don't reuse preview tab
     end,
+    ft = { "markdown" },
   },
 }
